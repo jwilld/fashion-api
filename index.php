b8ba12b87ac4f8b8085c1d9551e922c9e1231ff1
Jerry Davidson <willdjerry@gmail.com>
2019-09-13 16:02:56 -0400

    .htaccess

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $request = $_SERVER['REQUEST_URI'];
 
     switch ($request) {
-        case '/register' :
+        case '/' :
             require __DIR__ . '/php-jwt-example/api/register.php';
             break;
         case '/login' :
