70cb480b10f6cb57e95e55141a3e8dceda512fdb
Jerry Davidson <willdjerry@gmail.com>
2019-09-13 15:56:21 -0400

    router added to index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-    echo 'hello world'
+    $request = $_SERVER['REQUEST_URI'];
+
+    switch ($request) {
+        case '/register' :
+            require __DIR__ . '/php-jwt-example/api/register.php';
+            break;
+        case '/login' :
+            require __DIR__ . '/php-jwt-example/api/login.php';
+            break;
+    }
 
     ?>
