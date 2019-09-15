930553ef65a5111ab1bf6856fb1d9a526f48f48e
Jerry Davidson <willdjerry@gmail.com>
2019-09-15 10:48:08 -0400

    post.php accepts a post requesst

--- tables/tables.sql
+++ tables/tables.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS post(
-    id INT NOT NULL,
+    id INT(120) AUTO_INCREMENT,
     title VARCHAR(20),
     photographer VARCHAR(30),
+    photo VARCHAR(200),
     area VARCHAR(30),
-    id_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
 );
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT ,
     first_name VARCHAR(150) NOT NULL , 
     last_name VARCHAR(150) NOT NULL,
-    email VARCHAR(255),
-    password VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
